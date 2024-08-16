9224102f9522575d02405883ca840b0f3f63808b
TiagoM13 <thyagomotha2000@gmail.com>
2024-08-16 10:26:46 -0300

    feat: add adjusts to tests

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,10 +10,15 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      imports: [
+        LoginComponent,
+        HttpClientTestingModule,
+        ToastrModule.forRoot()
+      ],
+      providers: [ToastrService]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/pages/signup/signup.component.spec.ts
+++ src/app/pages/signup/signup.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignupComponent } from './signup.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 describe('SignupComponent', () => {
   let component: SignupComponent;
@@ -8,7 +10,12 @@ describe('SignupComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SignupComponent]
+      imports: [
+        SignupComponent,
+        HttpClientTestingModule,
+        ToastrModule.forRoot()
+      ],
+      providers: [ToastrService]
     })
       .compileComponents();
 
